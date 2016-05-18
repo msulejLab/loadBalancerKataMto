31fc13f8ef297091a23c0b1d14d59c6d69adc54e
Maciej Sulej <187809@edu.p.lodz.pl>
2016-05-19 00:57:15 +0200

    test1 passed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -16,8 +17,32 @@ public class ServerLoadBalancerTest {
     public void balancingServer_noVm_ServerStaysEmpty() {
         Server theServer = a(server().withCapacity(1));
 
-        balancing(aServersListWith(theServer), anEmptyListOfVms());
+        balance(aListOfServersWith(theServer), anEmptyListOfVms());
 
         assertThat(theServer, hasCurrentLoadOfVms(0.0d));
+    }
+
+    private Matcher<? super Server> hasCurrentLoadOfVms(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
+
+    private void balance(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers, vms);
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server[] aListOfServersWith(Server server) {
+        return new Server[] {server};
+    }
+
+    private Server a(ServerBuilder builder) {
+        return builder.build();
+    }
+
+    private ServerBuilder server() {
+        return new ServerBuilder();
     }
 }

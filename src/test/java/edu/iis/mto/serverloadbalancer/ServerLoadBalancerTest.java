2c180184adf4bfc68545005e508e618fad696266
Maciej Sulej <187809@edu.p.lodz.pl>
2016-05-19 02:16:38 +0200

    test4 failed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -46,6 +46,19 @@ public class ServerLoadBalancerTest {
         assertThat("server should contain vm", theServer.contains(theVm));
     }
 
+    @Test
+    public void balancingAServerWithEnoughRoom_getsFilledWithAllVms() {
+        Server theServer = a(server().withCapacity(100));
+        Vm theFirstVm = a(vm().ofSize(1));
+        Vm theSecondVm = a(vm().ofSize(1));
+
+        balancing(aListOfServersWith(theServer), aListOfVmsWith(theFirstVm, theSecondVm));
+
+        assertThat(theServer, hasAVmsCountOf(2));
+        assertThat("server should contain first vm", theServer.contains(theFirstVm));
+        assertThat("server should contain second vm", theServer.contains(theSecondVm));
+    }
+
     private void balancing(Server[] servers, Vm[] vms) {
         new ServerLoadBalancer().balance(servers, vms);
     }

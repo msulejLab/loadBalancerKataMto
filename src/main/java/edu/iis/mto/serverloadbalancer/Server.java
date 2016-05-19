a2da93c2845409657860469b5d5824215b91d9cd
Maciej Sulej <187809@edu.p.lodz.pl>
2016-05-19 02:01:10 +0200

    test3 refactored

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,6 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
 public class Server {
+    public static final int MAXIMUM_LOAD = 100;
+
     public int capacity;
     public double currentLoadPercentage;
 
@@ -10,5 +12,9 @@ public class Server {
 
     public boolean contains(Vm theVm) {
         return true;
+    }
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage = ((double) vm.size / capacity) * MAXIMUM_LOAD;
     }
 }
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,7 +4,7 @@ public class ServerLoadBalancer {
 
     public void balance(Server[] servers, Vm[] vms) {
         if (vms.length > 0) {
-            servers[0].currentLoadPercentage = ((double) vms[0].size / servers[0].capacity) * 100;
+            servers[0].addVm(vms[0]);
         }
     }
 }

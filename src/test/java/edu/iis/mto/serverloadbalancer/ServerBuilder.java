7ade281d1a797081d759f57860c1e1927a5e6f6f
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-15 18:15:53 +0200

    test 5 passed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
+
 public class ServerBuilder implements Builder<Server> {
 
     private int capacity;
+    private int initLoad;
 
     public ServerBuilder withCapacity(int capacity) {
         this.capacity = capacity;
@@ -10,10 +13,23 @@ public class ServerBuilder implements Builder<Server> {
     }
 
     public Server build() {
-        return new Server(capacity);
+        Server theServer = new Server(capacity);
+
+        if (initLoad > 0) {
+            int initVmSize = (int) (initLoad / (double) capacity * 100.0);
+            Vm initVm = vm().ofSize(initVmSize).build();
+            theServer.addVm(initVm);
+        }
+
+        return theServer;
     }
 
     public static ServerBuilder server() {
         return new ServerBuilder();
+    }
+
+    public ServerBuilder withCurrentLoadOf(int initLoad) {
+        this.initLoad = initLoad;
+        return this;
     }
 }

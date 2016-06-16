2d2434463e67dde5283f6614f02f4767f24b90bc
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-16 11:07:18 +0200

    vm balanced to less loaded server - passed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -2,14 +2,27 @@ package edu.iis.mto.serverloadbalancer;
 
 public class ServerBuilder implements Builder<Server> {
     private int capacity;
+    private double initLoad;
 
     public ServerBuilder withCapacity(int capacity) {
         this.capacity = capacity;
         return this;
     }
 
+    public ServerBuilder withCurrentLoadPercentage(double initLoadPercentage) {
+        this.initLoad = initLoadPercentage;
+        return this;
+    }
+
     public Server build() {
-        return new Server(capacity);
+        Server server = new Server(capacity);
+        if (initLoad > 0) {
+            int initVmSize = (int) ((initLoad / (double) capacity) * 100.0);
+            Vm initVm = new Vm(initVmSize);
+            server.addVm(initVm);
+        }
+
+        return server;
     }
 
     public static ServerBuilder server() {

47374901226ef7c449af3861c2f237295c5ba7ba
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-16 10:04:49 +0200

    one server gets filled with one vm - failed

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -21,6 +21,17 @@ public class ServerLoadBalancerTest {
         assertThat(theServer, CurrentLoadPercentageMatcher.hasCurrentLoadPercentageOf(0.0d));
     }
 
+    @Test
+    public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillTheServerWithTheVm() {
+        Server theServer = a(ServerBuilder.server().withCapacity(1));
+        Vm theVm = a(vm().ofSize(1));
+
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, CurrentLoadPercentageMatcher.hasCurrentLoadPercentageOf(100.0));
+        assertThat("server should contain vm", theServer.contains(theVm));
+    }
+
     private void balance(Server[] servers, Vm[] vms) {
         new ServerLoadBalancer().balance(servers, vms);
     }

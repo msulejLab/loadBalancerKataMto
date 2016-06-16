9c5a1f57e28635ef3c4f3e8c0fefb1ef6a1e53b4
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-16 09:59:34 +0200

    no vm server stays empty - passed

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class ServerBuilder {
+    public ServerBuilder withCapacity(int capacity) {
+        return this;
+    }
+
+    public Server build() {
+        return new Server();
+    }
+}
